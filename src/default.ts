290a338d00053e739c9af9727ed057261bb76184
ljj <893763507@qq.com>
2023-03-14 16:07:17 +0800

    update

---
+++ src/default.ts
@@ -0,0 +1,11 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiModel: "",
+  openaiAPITemperature: 70,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- Shift + Enter 换行。开头输入 / 或者 空格 搜索 Prompt 预设。点击输入框滚动到底部。`
