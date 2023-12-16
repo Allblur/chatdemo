a22b086a4d5e421c28c2e5942f3a9e365ebbc88f
ljj <893763507@qq.com>
2023-12-16 12:34:20 +0800

    update

--- src/pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from "astro"
-
-export const post: APIRoute = async context => {
-  const body = await context.request.json()
-  console.log("request body === ", body)
-  return new Response("test")
-}
